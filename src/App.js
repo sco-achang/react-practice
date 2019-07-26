4219a1cc76c91350b473bf1600927e549e9f9023
Albert Chang <achang@sco.ca.gov>
2019-07-26 12:15:57 -0700

    conditional rendering.

--- src/App.js
+++ src/App.js
@@ -3,17 +3,7 @@ import './App.css';
 
 /** Components */
 // import { Greet } from './components/Greet'; // name exports - HAS to be SAME
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
-import Message from './components/Message';
-import Counter from './components/Counter';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-import ParentComponent from './components/ParentComponent';
-import StyleSheet from './components/StyleSheet';
-import Inline from './components/Inline';
-// Form
-import Form from './components/Form';
+import UserGreeting from './components/UserGreeting';
 
 /** Styles */
 import './appStyles.css';
@@ -22,26 +12,7 @@ import styles from './appStyles.module.css';
 function App() {
   return (
     <div className="App">
-    <h1 className="error">Error</h1>
-    <h1 className={styles.success}>Success</h1>
-    {/* <StyleSheet primary="true"/>
-    <Inline /> */}
-    {/* <ParentComponent /> */}
-    {/* <FunctionClick></FunctionClick> */}
-    {/* <Counter></Counter> */}
-    {/* <Message /> */}
-      {/* GREET components */}
-      {/* <Greet name="Bruce" heroName="Batman">
-        <p>This is children props</p>
-      </Greet>
-      <Greet name="Diana" heroName="Wonderwoman"/> */}
-      
-      {/* WELCOME components */}
-      {/* <Welcome name="Bruce" heroName="Batman">
-        <p>This is children props</p>
-      </Welcome>
-      <Welcome name="Clark" heroName="Superman"/>
-      <Welcome name="Diana" heroName="Wonderwoman"/> */}
+      <UserGreeting />
     </div>
   );
 }
